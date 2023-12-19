dfa71300d0ce1f3bac0485a2eb11a28b804cf9fc
anasbjt <boujatouy@icloud.com>
2023-12-19 22:26:23 +0100

    first commit

---
+++ Projet/projetrest/src/main/java/com/projetrest/resources/CreneauSeanceRessource.java
@@ -0,0 +1,13 @@
+package com.projetrest.resources;
+
+import javax.ws.rs.Path;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+public class CreneauSeanceRessource {
+
+}
